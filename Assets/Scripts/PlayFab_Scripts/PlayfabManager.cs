dd63fc5b83f477547ad20936f1c2c6e69067a293
H4nzo <mtminimah@griotstudios.org>
2023-04-27 16:27:00 +0100

    PlayFab Custom ID Login Integration

--- Assets/Scripts/PlayFab_Scripts/PlayfabManager.cs
+++ Assets/Scripts/PlayFab_Scripts/PlayfabManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PlayFab;
 using PlayFab.ClientModels;
 
 public class PlayfabManager : MonoBehaviour
@@ -8,12 +9,35 @@ public class PlayfabManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Login()
     {
-        
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
     }
+
+    void OnSuccess(LoginResult result){
+        Debug.Log($"Successful login/account creation!");
+    }
+
+    void OnError(PlayFabError error){
+        Debug.Log($"Error while loggin in/ creating account");
+        Debug.Log(error.GenerateErrorReport());
+    }
+
+
+
+
+
+
+
+
+
+
 }
